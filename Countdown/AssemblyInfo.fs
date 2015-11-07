011e410e7a869ff4051948804c399566fab59403
Matt Becker <matt@mbecker.co.uk>
2015-11-07 01:57:34 -0800

    Create solution and GTK# project

---
+++ Countdown/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module Countdown.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("Countdown")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("matt")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ Countdown/Main.fs
@@ -0,0 +1,14 @@
+﻿namespace Countdown
+
+module Main = 
+    open System
+    open Gtk
+    
+    [<EntryPoint>]
+    let Main(args) = 
+        Application.Init()
+        let win = new MainWindow.MyWindow()
+        win.Show()
+        Application.Run()
+        0
+
