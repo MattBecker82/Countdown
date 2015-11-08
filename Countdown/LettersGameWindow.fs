0ca8897646c0f780bfeff62ee3b7a33afc2ec8d2
Matt Becker <matt@mbecker.co.uk>
2015-11-08 11:09:59 -0800

    Add (empty) Letters Game window

---
+++ Countdown/LettersGameWindow.fs
@@ -0,0 +1,10 @@
+﻿namespace Countdown
+
+module LettersGameWindow =
+    open System
+    open Gtk
+    
+    type LettersGameWindow() as this = 
+        inherit Window("Letters Game")
+        do this.SetDefaultSize(400, 300)
+        do this.ShowAll()
--- Countdown/MainWindow.fs
+++ Countdown/MainWindow.fs
@@ -5,10 +5,21 @@ module MainWindow =
     open Gtk
     
     type MyWindow() as this = 
-        inherit Window("MainWindow")
+        inherit Window("Countdown")
         do this.SetDefaultSize(400, 300)
         do this.DeleteEvent.AddHandler(fun o e -> this.OnDeleteEvent(o, e))
+        let lettersButton = new Button("Letters")
+        do lettersButton.Clicked.AddHandler(fun o e -> this.OnLettersClicked(o, e))
+        do this.Add(lettersButton)
         do this.ShowAll()
+
         member this.OnDeleteEvent(o, e : DeleteEventArgs) = 
             Application.Quit()
             e.RetVal <- true
+
+        member this.OnLettersClicked(o, e) =
+            let win = new Countdown.LettersGameWindow.LettersGameWindow()
+            do win.Show()
+
+
+        
